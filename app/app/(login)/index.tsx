1e6bb7d42b6b3fd432abcfeb64b44a2ffb12fe68
yuapi <phk2225@gmail.com>
2024-08-22 02:07:18 +0900

    카카오 로그인 -> 구글 로그인 Amplify, Cognito 도입

--- app/app/(login)/index.tsx
+++ app/app/(login)/index.tsx
@@ -1,96 +1,89 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, TouchableOpacity, Image } from 'react-native';
-import * as AuthSession from 'expo-auth-session';
+import { Button, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import { Text, View } from '@/components/Themed';
-import { kakao_api_key } from '@/config.json';  
 import { Link } from 'expo-router';
+import * as Linking from 'expo-linking';
+import { Amplify } from 'aws-amplify';
+import { Hub } from "@aws-amplify/core";
+import { AuthUser, getCurrentUser, signInWithRedirect, signOut } from '@aws-amplify/auth';
+import { cognito } from '@/config.json';
 
-const REDIRECT_URI = AuthSession.makeRedirectUri();
-const KAKAO_AUTH_URL = 'https://kauth.kakao.com/oauth/authorize';
-const TOKEN_URL = 'https://kauth.kakao.com/oauth/token';
-const USER_INFO_URL = 'https://kapi.kakao.com/v2/user/me';
-
-// Discovery Document (replace with the correct configuration if available)
-const DISCOVERY_DOC = {
-  authorizationEndpoint: KAKAO_AUTH_URL,
-  tokenEndpoint: TOKEN_URL,
-  revocationEndpoint: '', // You can leave it empty if not used
-};
-
-interface UserInfo {
-	properties: {
-	  nickname: string;
-	  profile_image: string;
-	};
-	kakao_account: { // 변경
-	  email: string;
-	};
-  }  
+Amplify.configure({
+	Auth: {
+		Cognito: {
+			userPoolClientId: cognito.userPoolClientId,
+			userPoolId: cognito.userPoolId,
+			loginWith: {
+				oauth: {
+					domain: cognito.domain,
+					scopes: ['openid', 'email', 'profile'],
+					redirectSignIn: [Linking.createURL('')],
+					redirectSignOut: [Linking.createURL('')],
+					responseType: 'code'
+				}
+			}
+		}
+	}	
+})
 
 export default function LoginScreen() {
-	const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+	const [user, setUser] = useState<AuthUser | null>(null);
+	const [error, setError] = useState<unknown>(null);
+	const [customState, setCustomState] = useState<string | null>(null);
 
-	// Create an AuthRequest instance
-	const [request, response, promptAsync] = AuthSession.useAuthRequest(
-	  {
-      clientId: kakao_api_key,
-	  scopes: ['profile_nickname', 'profile_image', 'account_email'], // 변경
-      redirectUri: REDIRECT_URI,
-      responseType: 'code',
-	  },
-	  DISCOVERY_DOC
-	);
-  
 	useEffect(() => {
-	  if (response?.type === 'success' && response.params.code) {
-		const fetchToken = async () => {
-		  const tokenResponse = await fetch(TOKEN_URL, {
-			method: 'POST',
-			headers: {
-			  'Content-Type': 'application/x-www-form-urlencoded',
-			},
-			body: `grant_type=authorization_code&client_id=${kakao_api_key}&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&code=${response.params.code}`,
-		  });
-  
-		  const { access_token } = await tokenResponse.json();
-  
-		  const userResponse = await fetch(USER_INFO_URL, {
-			headers: {
-			  Authorization: `Bearer ${access_token}`,
-			},
-		  });
-  
-		  const user: UserInfo = await userResponse.json();
-		  setUserInfo(user);
-		};
-  
-		fetchToken();
-	  }
-	}, [response]);
+		const unsubscribe = Hub.listen("auth", ({ payload }) => {
+			switch (payload.event) {
+				case "signInWithRedirect":
+				getUser();
+				break;
+				case "signInWithRedirect_failure":
+				setError("An error has occurred during the OAuth flow.");
+				break;
+				case "customOAuthState":
+				setCustomState(payload.data); // this is the customState provided on signInWithRedirect function
+				break;
+			}
+		});
+	
+		getUser();
 
-  return (
-    <View style={styles.container}>
-      <View style={styles.logoContainer}>
-        <Image source={require('../../assets/images/logo.jpg')} style={styles.logo} />
-        <Text style={styles.title}>TravelMakers</Text>
-        <Text style={styles.subtitle}>어디로 떠날지 모르겠을땐?</Text>
-      </View>
-      {userInfo ? (
-        <View style={styles.userInfo}>
-          <Image source={{ uri: userInfo.properties.profile_image }} style={styles.profileImage} />
-          <Text style={styles.userName}>{userInfo.properties.nickname}</Text>
-		  <Text style={styles.userEmail}>{userInfo.kakao_account.email}</Text>   {/* 변경 */}
-        </View>
-      ) : (
-        <TouchableOpacity style={styles.loginButton} onPress={() => promptAsync()}>
-          <Image source={require('../../assets/images/kakao-login.png')} style={styles.kakaoLogo} resizeMode="contain" />
-        </TouchableOpacity>
-      )}
-      <Text style={styles.footerText}>SNS 계정으로 간편 가입</Text>
-      <Text style={styles.chatbotText}>CHAT BOT AI TRAVEL SERVICE</Text>
-	  <Link href='/question1'>Question1 테스트</Link>
-    </View>
-  );
+		return unsubscribe;
+	}, []);
+
+	const getUser = async (): Promise<void> => {
+		try {
+			const currentUser = await getCurrentUser();
+			setUser(currentUser);
+		} catch (error) {
+			console.error(error);
+			console.log("Not signed in");
+		}
+	};
+
+	return (
+		<View style={styles.container}>
+			<View style={styles.logoContainer}>
+				<Image source={require('../../assets/images/logo.jpg')} style={styles.logo} />
+				<Text style={styles.title}>TravelMakers</Text>
+				<Text style={styles.subtitle}>어디로 떠날지 모르겠을땐?</Text>
+			</View>
+			{user ? (
+				<View style={styles.userInfo}>
+				<Text style={styles.userName}>{user.username}</Text>
+				<Text style={styles.userName}>{user.userId}</Text>
+				<Button title="Sign Out" onPress={() => {signOut(); setUser(null)}}></Button>
+				</View>
+			) : (
+				<TouchableOpacity style={styles.loginButton} onPress={() => signInWithRedirect({provider: 'Google'})}>
+				<Image source={require('../../assets/images/web_light_sq_ctn.png')} resizeMode="contain" />
+				</TouchableOpacity>
+			)}
+			<Text style={styles.footerText}>SNS 계정으로 간편 가입</Text>
+			<Text style={styles.chatbotText}>CHAT BOT AI TRAVEL SERVICE</Text>
+			<Link href='/question1'>Question1 테스트</Link>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
@@ -120,17 +113,10 @@ const styles = StyleSheet.create({
 		marginBottom: 30,
 	  },
 	  loginButton: {
-		width: 200,
-		height: 50,
 		justifyContent: 'center',
 		alignItems: 'stretch',
-		backgroundColor: '#FEE500',
 		borderRadius: 5,
 		marginBottom: 20,
-	  },
-	  kakaoLogo: {
-		width: 200, 
-		height: 40, 
 	  },
 	  userInfo: {
 		alignItems: 'center',
